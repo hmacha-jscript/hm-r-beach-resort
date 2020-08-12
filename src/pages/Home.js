643910a94dc28bbdfe3ccaebdbad97cdda202360
RAJ <rajumacha@yahoo.com>
2020-08-12 17:35:57 +0530

    rendering banners

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 
 import React from 'react';
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <Hero hero="defaultHero" />
+        <Hero>
+            <Banner
+                title="Luxurious Rooms"
+                subtitle="Deluxe Rooms starting @990">
+                <Link to="/rooms" className="btn-primary">our rooms</Link>
+            </Banner>
+        </Hero>
     )
 }
 
