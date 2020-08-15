4b7c0fbd2d22b4fe66d9747d403fa2d0ead11ec9
RAJ <rajumacha@yahoo.com>
2020-08-15 16:31:14 +0530

    Single Room login is in progress

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import defaultBcg from '../images/room-1.jpeg';
-import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import { RoomContext } from '../context';
+import StyledHero from '../components/StyledHero';
 
 class SingleRoom extends Component {
 
@@ -19,9 +19,37 @@ class SingleRoom extends Component {
     render() {
         const { getRoom } = this.context;
         const room = getRoom(this.state.slug)
-        console.log(room)
+        if (!room) {
+            return (
+                <div className="error">
+                    <h3>No Such Room could be found..</h3>
+                    <Link to="/rooms" className="btn-primary">Back to Rooms</Link>
+                </div>
+            )
+        }
+
+        // const { name, description, capacity, size, price, extras, breakfast, pets, images } = room;
+        const { name, images } = room;
+
         return (
-            <div>This is SingleRoom</div>
+            <>
+                <StyledHero img={images[0]}>
+                    <Banner title={`${name} room`}>
+                        <Link to="/rooms" className="btn-primary">
+                            back to rooms
+                    </Link>
+                    </Banner>
+                </StyledHero>
+                <section className="single-room">
+                    <div className="single-room-images">
+                        {
+                            images.map((item, index) => {
+                                return <img key={index} src={item} alt={name} />
+                            })
+                        }
+                    </div>
+                </section>
+            </>
         )
     }
 }
